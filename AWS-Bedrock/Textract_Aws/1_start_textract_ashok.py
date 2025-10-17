4efb7c39be38fac6cf065c575c96027b1b933356
Ashok Mulchandani <ashokmulchandani@gmail.com>
2025-10-17 11:20:58 +1100

    Add Textract starter Lambda with dynamic account ID detection

---
+++ AWS-Bedrock/Textract_Aws/1_start_textract_ashok.py
@@ -0,0 +1,29 @@
+import boto3
+import json
+
+def lambda_handler(event, context):
+    textract = boto3.client('textract')
+    
+    # Get AWS account ID and region dynamically
+    account_id = context.invoked_function_arn.split(':')[4]
+    region = context.invoked_function_arn.split(':')[3]
+    
+    # Get S3 event details
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    
+    # Start Textract job
+    response = textract.start_document_text_detection(
+        DocumentLocation={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key
+            }
+        },
+        NotificationChannel={
+            'SNSTopicArn': f'arn:aws:sns:{region}:{account_id}:textract-completion-topic',
+            'RoleArn': f'arn:aws:iam::{account_id}:role/TextractServiceRole'
+        }
+    )
+    
+    return {'statusCode': 200, 'body': f"Started job: {response['JobId']}"}
